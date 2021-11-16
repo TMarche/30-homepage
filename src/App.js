bc7d9564c54d13d1e6599aeebd107943683da674
Troy Marchesseault <tmarchess@gmail.com>
2021-11-15 19:48:14 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <span>Hi! </span>
+            I'm Troy Marchesseault and I'm making
+            <span> 30 projects in 30 days. </span>
+            Follow the project here!
+        </div>
+    );
 }
 
 export default App;
