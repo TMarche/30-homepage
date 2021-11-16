3ff2358e2dc2734d7ac43c9e8abc310784020c60
Troy Marchesseault <tmarchess@gmail.com>
2021-11-15 20:16:42 -0500

    Map over project names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { projects } from "./data/data";
+
 function App() {
     return (
         <div className="App">
+            <h1>30 Projects in 30 Days</h1>
             <span>Hi! </span>
             I'm Troy Marchesseault and I'm making
             <span> 30 projects in 30 days. </span>
-            Follow the project here!
+            Follow the progress here!
+            <ol>
+                {projects.map((projectName) => (
+                    <li>{projectName}</li>
+                ))}
+            </ol>
         </div>
     );
 }
